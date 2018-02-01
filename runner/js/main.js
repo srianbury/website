8c062670b8a673cd4e1e341925f6bab6921c6c40
Brian Sunbury <bsunbury29@gmail.com>
2018-02-01 14:49:35 -0600

    removed runner, placed in new repo

--- runner/js/main.js
+++
@@ -1,8 +0,0 @@
-//creates an instance of the game
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '')
-
-game.state.add('Boot', StanRunner.Boot);
-game.state.add('Preloader', StanRunner.Preload);
-game.state.add('MainMenu', StanRunner.MainMenu);
-
-game.state.start('Boot');
